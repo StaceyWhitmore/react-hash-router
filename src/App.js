c3306058709bc853aa31919fb196d6e6e3c4f8c1
StaceyWhitmore <staceywhitmore@gmail.com>
2018-11-13 18:43:06 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+export const Home = () =>
+  <section className="home">
+    <h1>Home Page</h1>
+  </section>
 
-export default App;
+  export const About = () =>
+    <section className="about">
+      <h1>About</h1>
+    </section>
+
+export const Events = () =>
+  <section className="events">
+    <h1>Events</h1>
+  </section>
+
+export const Products = () =>
+  <section className="products">
+    <h1>Products</h1>
+  </section>
+
+export const Contact = () =>
+  <section className="contact">
+    <h1>Contact Us</h1>
+  </section>
