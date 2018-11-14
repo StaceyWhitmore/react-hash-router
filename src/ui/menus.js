c935493aabdfb0306c951795e5ac153e091ee00e
StaceyWhitmore <staceywhitmore@gmail.com>
2018-11-13 19:02:18 -0700

    added some directory structur. Started menu

---
+++ src/ui/menus.js
@@ -0,0 +1,8 @@
+import HomeIcon from 'react-icons/lib//fa/home'
+import { NavLink } from 'react-router-dom'
+//import './stylesheets/menu.scss'
+
+const selectedStyle = {
+  background: "white",
+  color: "slategray"
+}
